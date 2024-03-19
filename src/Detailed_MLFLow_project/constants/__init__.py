a209f821b144eb449ca9373f326da26461ded267
hrishikesh147 <hrishikeshbhagawati@gmail.com>
2024-03-19 20:43:32 +0530

    data validation added

--- src/Detailed_MLFLow_project/constants/__init__.py
+++ src/Detailed_MLFLow_project/constants/__init__.py
@@ -3,3 +3,8 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path("config.yaml")
 SCHEMA_FILE_PATH = Path("schema.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
+
+#CONFIG_FILE_PATH = "E:\\FullStack_Data\\MACHINE_LEARNING\\PROJECTS\\Detailed_Project\\Detailed_MLFLOW\\config.yaml"
+
+
+
